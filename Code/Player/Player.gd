e1116036ddb142501e7a65f6f0678f4402e534a5
unblinky <47279470+unblinky@users.noreply.github.com>
2023-08-16 20:25:53 -0700

    Character collisions started,

--- Code/Player/Player.gd
+++ Code/Player/Player.gd
@@ -1,19 +1,15 @@
 extends CharacterBody2D
 class_name Player
 
-
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	var direction = Input.get_axis("move_left", "move_right")
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
