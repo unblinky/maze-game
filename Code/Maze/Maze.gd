e2bc021f28077d6fbb35a3574d75f34a0db62685
unblinky <47279470+unblinky@users.noreply.github.com>
2023-08-21 20:43:25 -0700

    Got tile set layers. Cleanup on enums.

--- Code/Maze/Maze.gd
+++ Code/Maze/Maze.gd
@@ -8,8 +8,11 @@ const ROOM = preload("res://Room/Room.tscn")
 
 
 @export var size: Vector2i = Vector2i(3, 4)
-@export var separation: int = 128
+@export var separation: int = 64
 
+var maze_offset: Vector2i = Vector2i(128, 128)
+var rooms: Array[Room]
+var visited_rooms: Array[Room]
 
 
 func _ready():
@@ -22,8 +25,43 @@ func CreateMaze():
 			await timer.timeout
 			
 			var room = ROOM.instantiate()
-			room.position.x = width_index * separation
-			room.position.y = height_index * separation
+			visited_rooms.append(room)
+			
 			add_child(room)
 			
+			var new_door: Room.Door = room.RandomDirection()
+			match new_door:
+				Room.Door.NORTH:
+					room.ShowDoor(Room.Door.NORTH)
+					height_index -= 1
+				Room.Door.EAST:
+					room.ShowDoor(Room.Door.EAST)
+					width_index += 1
+				Room.Door.SOUTH:
+					room.ShowDoor(Room.Door.SOUTH)
+					height_index += 1
+				Room.Door.WEST:
+					room.ShowDoor(Room.Door.WEST)
+					width_index -= 1
 			
+			room.position.x = width_index * separation + maze_offset.x
+			room.position.y = height_index * separation + maze_offset.y
+			
+			
+			
+			# Handle the width.
+			if width_index <= 0:
+				room.HideDoor(Room.Door.WEST)
+			if width_index > size.x - 2:
+				room.HideDoor(Room.Door.EAST)
+			
+			# Handle the height.
+			if height_index <= 0:
+				room.HideDoor(Room.Door.NORTH)
+			if height_index > size.y - 2:
+				room.HideDoor(Room.Door.SOUTH)
+			rooms.append(room)
+	
+	rooms[0].ShowDoor(Room.Door.NORTH)
+	#for room in Rooms:
+		
--- Code/Room/Room.gd
+++ Code/Room/Room.gd
@@ -1,4 +1,54 @@
 extends Node2D
 class_name Room
 
+@onready var north_door = $NorthDoor
+@onready var east_door = $EastDoor
+@onready var south_door = $SouthDoor
+@onready var west_door = $WestDoor
 
+enum Door
+{
+	NORTH,
+	EAST,
+	SOUTH,
+	WEST
+}
+
+var doors: Array[Door] = [Door.NORTH, Door.EAST, Door.SOUTH, Door.WEST]
+
+
+func RandomDirection():
+	var rando: int = randi_range(0, doors.size() - 1)
+	return doors.pop_at(rando)
+
+
+func ShowDoor(door: Door):
+	match door:
+		Door.NORTH:
+			north_door.show()
+			doors.append(Door.NORTH)
+		Door.EAST:
+			east_door.show()
+			doors.append(Door.EAST)
+		Door.SOUTH:
+			south_door.show()
+			doors.append(Door.SOUTH)
+		Door.WEST:
+			west_door.show()
+			doors.append(Door.WEST)
+
+
+func HideDoor(door: Door):
+	match door:
+		Door.NORTH:
+			north_door.hide()
+			doors.erase(Door.NORTH)
+		Door.EAST:
+			east_door.hide()
+			doors.erase(Door.EAST)
+		Door.SOUTH:
+			south_door.hide()
+			doors.erase(Door.SOUTH)
+		Door.WEST:
+			west_door.hide()
+			doors.erase(Door.WEST)
